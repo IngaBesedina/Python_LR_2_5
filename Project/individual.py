9542bdd620cce335848d82d171a8e491dc6eaa90
IngaBesedina <inga.besedina@mail.ru>
2023-12-12 00:01:08 +0300

    edit py files

--- Project/individual.py
+++ Project/individual.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 if __name__ == '__main__':
-    A = tuple(map(int, input().split()))
+    t = tuple(map(int, input().split()))
 
-    k = A.count(A[0])
-    print("Количество равных элементов:", k)
-    print("Элементы, следующие за равными элементами: ", A[k:len(A)+1])
+    c = t.count(t[0])
+    print("Количество равных элементов:", c)
+    print("Элементы, следующие за последним из них:", t[c:])
